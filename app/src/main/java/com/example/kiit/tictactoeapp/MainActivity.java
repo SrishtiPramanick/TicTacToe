534640d6f5e0abee9ac7ae8cec541878f99241f8
Srishti Pramanick <1706275@kiit.ac.in>
2020-03-11 23:33:51 +0530

    Commiting files

---
+++ app/src/main/java/com/example/kiit/tictactoeapp/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.kiit.tictactoeapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void play(View view) {
+        Intent i=new Intent(this,TictactoeActivity.class);
+        startActivity(i);
+    }
+
+    public void exit(View view) {
+        finish();
+    }
+}
